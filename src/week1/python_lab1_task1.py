fa1a56c369cfa1766d8d0dd734beb43732cc39d7
leylilee <leyli.iskandarli@gmail.com>
2025-10-27 08:48:51 +0000

    tasks 1 and 2

--- src/week1/python_lab1_task1.py
+++ src/week1/python_lab1_task1.py
@@ -8,12 +8,16 @@ Ask user for radius and print result with 2 decimals.
 """
 
 # TODO: import math
+import math
 
 def circle_area(radius):
     """Return the area of a circle given its radius."""
     # TODO: implement formula using math.pi
-    pass
+    return math.pi * r**2
+
 
 if __name__ == "__main__":
     # TODO: ask for user input, call circle_area(), and print formatted result
-    pass
+    r = float(input("Enter the radius: "))
+    area = circle_area(r)
+    print(f"The Area is {area:.2f}")
--- src/week1/python_lab1_task2.py
+++ src/week1/python_lab1_task2.py
@@ -11,8 +11,11 @@ Ask user for their name and print result.
 def greet_user(name):
     """Return a greeting message after cleaning and capitalizing the name."""
     # TODO: implement cleaning and formatting
-    pass
+    new_name = name.strip().capitalize()
+    return f"Hello, {new_name}! Welcome to Python!"
+
 
 if __name__ == "__main__":
     # TODO: read name from input and print greeting
-    pass
+    name = input("Enter your name: ")
+    print(greet_user(name))
